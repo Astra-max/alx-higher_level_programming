055c6b970bbfab5832ae80d57c7d8b49ac5907e7
Astra-max <waoremaxwel17@gmail.com>
2023-11-29 01:59:27 +0300

    FizzBuzz game

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,17 +1,20 @@
-#!/usr/bin/python3
+##!/usr/bin/python3
 
 def fizzbuzz():
-    for game in range(1, 101):
-        if game % 3 == 0 and game % 5 == 0:
-           print("FizzBuzz", end=" ")
-        elif game % 3 == 0:
-           print("Fizz")
-        elif game % 5 == 0 and game % 5 == 0:
-           print("Buzz", end=" ")
+    """
+    Prints numbers from 1 to 100 with specific conditions.
+    """
+    for i in range(1, 101):
+        if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz", end=" ")
+        elif i % 3 == 0:
+            print("Fizz", end=" ")
+        elif i % 5 == 0:
+            print("Buzz", end=" ")
         else:
-            print("{}".format(game), end=" ")
+            print(i, end=" ")
 
 
 if __name__ == "__main__":
-             fizzbuzz()
-             print("")
+    fizzbuzz()
+    print("")
