da49f86c24f9d9933e335eeb73140412f2b18d11
Astra-max <waoremaxwel17@gmail.com>
2023-11-29 00:02:52 +0300

    working with numbers

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 
-for number in range(100):
-    if number < 10:
-        print("0{},{}".format(number, " "), end="")
-    elif number == 99:
-        print("{}".format(number))
-    else:
-        print("{},{}".format(number, " "), end="")
+def numbers(i):
+    for number in range(i):
+        if number < 99:
+            print("{:02d},".format(number), end=" ")
+        else:
+            print("{:02d}".format(number))
+
+
+numbers(100)
