1a8c1137f96f49e440488a14d55e2968e00c1d89
Astra-max <waoremaxwel17@gmail.com>
2023-12-06 23:18:34 +0300

    common items in set

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -13,4 +13,5 @@ def common_elements(set_1, set_2):
 if __name__ == '__main__':
     set_1 = {"Python", "C", "Javascript"}
     set_2 = {"Bash", "C", "Ruby", "Perl"}
-    print(common_elements(set_1, set_2))
+    c_set = common_elements(set_1, set_2)
+    print(sorted(list(c_set)))
