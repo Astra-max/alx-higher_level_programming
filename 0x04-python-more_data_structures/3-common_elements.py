85f897be6fdd676943416fcee6f88b0b23f36926
Astra-max <waoremaxwel17@gmail.com>
2023-12-06 23:30:57 +0300

    common items in set

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,17 +1,4 @@
 #!/usr/bin/python3
-
 def common_elements(set_1, set_2):
-    # returns common element in sets
-
-    common_item = set_1 & set_2
-    if common_item:
-        return list(common_item)
-    else:
-        return False
-
-
-if __name__ == '__main__':
-    set_1 = {"Python", "C", "Javascript"}
-    set_2 = {"Bash", "C", "Ruby", "Perl"}
-    c_set = common_elements(set_1, set_2)
-    print(sorted(list(c_set)))
+    common = set_1.intersection(set_2)
+    return common
