c8982a25db721dfdb2f98ac18c420cc47bb14f99
Astra-max <waoremaxwel17@gmail.com>
2023-12-06 22:48:09 +0300

    replacing items on list

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,20 +1,18 @@
 #!/usr/bin/python3
-
 def search_replace(my_list, search, replace):
     new_list = []
     # creates a new list
     for number in my_list:
         if number == search:
-           new_list.append(replace)
+            new_list.append(replace)
         else:
-           new_list.append(number)
-
+            new_list.append(number)
 
     return new_list
 
 
 if __name__ == '__main__':
     my_list = [1, 2, 3, 4, 5, 4, 2, 1, 1, 4, 89]
-    new_list = search_replace(my_list, 2, 89)    
+    new_list = search_replace(my_list, 2, 89)
     print(new_list)
     print(my_list)
