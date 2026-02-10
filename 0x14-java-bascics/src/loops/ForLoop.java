21496bc84ac8093481aa69d7a7ca8be382538d5b
Astra-max <waoremaxwel17@gmail.com>
2026-02-10 23:40:19 +0300

    basic array and for loop

---
+++ 0x14-java-bascics/src/loops/ForLoop.java
@@ -0,0 +1,13 @@
+public class ForLoop {
+    public static void main(String[] args) {
+        AllElemnts()
+    }
+
+    public void AllElemnts() {
+        int[10] nums = {1,2,3,4,5}
+
+        for (int val : nums) {
+            System.out.println(val)
+        }
+    }
+}
