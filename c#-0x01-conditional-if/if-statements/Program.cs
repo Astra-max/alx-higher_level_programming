e190eafd1943dd5718092889546c088d094e0664
Astra-max <waoremaxwel17@gmail.com>
2026-04-08 16:41:00 +0300

    if-else-if-else control flow

---
+++ c#-0x01-conditional-if/if-statements/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+namespace IfStatements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string? input = UserInput("What is your age?: ");
+            int age = Convert.ToInt32(input);
+
+            if (age >= 40)
+            {
+                Console.WriteLine("You are an adult.");
+            }
+            else if (age <= 18)
+            {
+                Console.WriteLine("You are a minor.");
+            }
+            else
+            {
+                Console.WriteLine("You are a young adult.");
+            }
+        }
+
+        static string? UserInput(string message)
+        {
+            Console.Write(message);
+            return Console.ReadLine();
+        }
+    }
+}
