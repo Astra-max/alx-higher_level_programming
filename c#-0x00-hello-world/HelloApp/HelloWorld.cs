a9cb4d649f450a59767ea474cff9437cb54b7094
Astra-max <waoremaxwel17@gmail.com>
2026-04-08 13:19:37 +0300

    hello world c# program

---
+++ c#-0x00-hello-world/HelloApp/HelloWorld.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace helloProgram
+{
+    class HelloWorld
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, maxwell!");
+        }
+    }
+}
