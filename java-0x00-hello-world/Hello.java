6ea29d4460760af3c7ff171bd0f3908630e640cb
Astra-max <waoremaxwel17@gmail.com>
2026-04-09 11:58:03 +0300

    java primitive variable

--- java-0x00-hello-world/Hello.java
+++ java-0x00-hello-world/Hello.java
@@ -1,5 +1,8 @@
+import primitive.PrimitiveVar;
+
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello, world!");
+        PrimitiveVar.primVariables();
     }
 }
