09672daca6a020892cdbf2a288233688fd74adf0
Astra-max <waoremaxwel17@gmail.com>
2026-04-10 23:56:58 +0300

    conditional switch statements

--- java-0x01-control-flow/src/Main.java
+++ java-0x01-control-flow/src/Main.java
@@ -6,6 +6,7 @@ public class Main {
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.println("Hello and welcome!");
         conditionIf();
+        switchStatements();
     }
     public static void conditionIf() {
         int myAge = 36;
@@ -16,6 +17,21 @@ public class Main {
             System.out.println("You are a teenage");
         } else {
             System.out.println("You are a teenager");
+        }
+    }
+
+    public static void switchStatements() {
+        String myName = "Astra";
+
+        switch (myName) {
+            case "Astra":
+                System.out.println("congratulations you got it!");
+                break;
+            case "James":
+                System.out.println("sorry try again!");
+                break;
+            default:
+                System.out.println("failed to guess my name");
         }
     }
 }
