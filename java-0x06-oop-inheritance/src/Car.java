f97498ad50159f9ea3affd1bda22ff29f9072d69
Astra-max <waoremaxwel17@gmail.com>
2026-04-11 02:02:27 +0300

    inheritance in java

---
+++ java-0x06-oop-inheritance/src/Car.java
@@ -0,0 +1,8 @@
+public class Car extends Vehicle {
+    String name;
+    String color;
+    public Car(String name, String color) {
+        this.name = name;
+        this.color = color;
+    }
+}
