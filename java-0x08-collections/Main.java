5f66a5726c493c5ae1bc20c398197657b28e98e8
Maxwel Waore <waoremaxwel17@gmail.com>
2026-04-22 12:12:34 +0100

    java: removing value from array list

--- java-0x08-collections/Main.java
+++ java-0x08-collections/Main.java
@@ -4,6 +4,9 @@ public class Main {
     public static void main(String[] args) {
         MyLists mylist = new MyLists();
         mylist.add("astra");
+        mylist.add("james");
+        mylist.add("john");
+        mylist.remove(2);
         mylist.print();
     }
 }
--- java-0x08-collections/src/lists/MyLists.java
+++ java-0x08-collections/src/lists/MyLists.java
@@ -4,13 +4,34 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class MyLists {
-    List<String> myList = new ArrayList<>();
+    private final List<String> myList = new ArrayList<>();
+    private String user;
+
     public void add(String user) {
         myList.add(user);
     }
+
     public void print() {
-        for (int i = 0; i< myList.size(); i++) {
+        for (int i = 0; i < myList.size(); i++) {
             System.out.printf("UserName: %s\n", myList.get(i));
+        }
+    }
+
+    public void remove(int itemId) {
+
+        for (int i = 0; i < myList.size(); i++) {
+            if (i == itemId) {
+                try {
+                    user = myList.get(itemId);
+                    myList.remove(itemId);
+                } catch (IndexOutOfBoundsException e) {
+                    System.out.printf("Failed to remove item: %s\n", e.getMessage());
+                    return;
+                }
+                finally {
+                    System.out.printf("removed %s succefully\n", user);
+                }
+            }
         }
     }
 }
