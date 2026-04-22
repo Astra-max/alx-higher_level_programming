565e0ccd6c7811a4923a60ee12fc25c7cdb04455
maodongo <odongo724@gmail.com>
2026-04-22 11:59:09 +0100

    java: adding values to array lists

---
+++ java-0x08-collections/Main.java
@@ -0,0 +1,9 @@
+import src.lists.MyLists;
+
+public class Main {
+    public static void main(String[] args) {
+        MyLists mylist = new MyLists();
+        mylist.add("astra");
+        mylist.print();
+    }
+}
