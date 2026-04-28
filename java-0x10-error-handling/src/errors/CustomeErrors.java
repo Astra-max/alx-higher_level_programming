32f78dd70a9b8ba43dc01a3421ee235db71fac6b
Astra-max <waoremaxwel17@gmail.com>
2026-04-28 17:48:40 +0100

    java: implemented custom error class by inherentence

---
+++ java-0x10-error-handling/src/errors/CustomeErrors.java
@@ -0,0 +1,7 @@
+package errors;
+
+public class CustomeErrors extends ArithmeticException {
+    public CustomeErrors(String message) {
+        super(message);
+    }
+}
--- java-0x10-error-handling/src/errors/ThrowingErrors.java
+++ java-0x10-error-handling/src/errors/ThrowingErrors.java
@@ -2,7 +2,9 @@ package errors;
 
 public class ThrowingErrors {
     public String party(int age) {
-        if (age < 18) throw new ArithmeticException("Sorry you are a minor");
+        if (age < 18) {
+            throw new CustomeErrors("Sorrry you are a minor");
+        }
         return null;
     }
 }
