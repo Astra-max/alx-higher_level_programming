1e0122c6453c884780c5c8c81ebf926dd79777ed
Astra-max <waoremaxwel17@gmail.com>
2026-04-28 10:42:04 +0100

    java: significance of java this key word

---
+++ java-0x05-object-oriented-prog/src/classses/This.java
@@ -0,0 +1,11 @@
+/**
+ * this binds a attribute to a class
+ * differentiates arguates with class attributes
+ */
+
+public class This {
+    String name;
+    This(String name) {
+        this.name = name;
+    }
+}
