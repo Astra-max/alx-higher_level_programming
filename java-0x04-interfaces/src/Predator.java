1170e341c5727471a471fc6a103c01fe8c12dd7f
Astra-max <waoremaxwel17@gmail.com>
2026-04-28 11:02:48 +0100

    java: abstraction/multiple inheritence with interfaces

--- java-0x04-interfaces/src/Predator.java
+++ java-0x04-interfaces/src/Predator.java
@@ -1,3 +1,11 @@
+/**
+ * interfaces - are abstract classes
+ * you cant creat object from it
+ * facilitates class abstraction
+ * useful in implementing multiple inheritence
+ * contains functions with empty bodies
+ */
+
 public interface Predator {
     void hunt();
 }
