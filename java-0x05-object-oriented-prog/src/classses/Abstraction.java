a96160aba486744071a74583e9b2df462550eaa2
Astra-max <waoremaxwel17@gmail.com>
2026-04-28 10:55:00 +0100

    java: accessing abstract classes and method

---
+++ java-0x05-object-oriented-prog/src/classses/Abstraction.java
@@ -0,0 +1,13 @@
+/**
+ * abstraction - hidding data from user/programs
+ * can be implemented by abstract classes/interfaces
+ * abstract class - cant create object from it
+ * abstract methods - only accessed in abstract class
+ *    ----> implemented by inheriting subclass
+ */
+
+
+abstract class Abstraction {
+    abstract String getUser();
+}
+
---
+++ java-0x05-object-oriented-prog/src/classses/ImplementAbs.java
@@ -0,0 +1,6 @@
+public class ImplementAbs extends Abstraction {
+    private String user = "astra6";
+    public String getUser() {
+        return this.user;
+    }
+}
