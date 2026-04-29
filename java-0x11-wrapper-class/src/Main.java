95a45782e520f920820f41ba62d2bd1c193b55b9
Astra-max <waoremaxwel17@gmail.com>
2026-04-30 01:43:56 +0300

    java: building objects on primitive type/wrapper classes

---
+++ java-0x11-wrapper-class/src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] arg) {
+        WrapperClass wc = new WrapperClass();
+        wc.concat();
+    }
+}
---
+++ java-0x11-wrapper-class/src/libs/WrapperClass.java
@@ -0,0 +1,15 @@
+
+/**
+ * Wrapper - gives us away to work with primitive type as objects
+ * all methods and obj implements, can work on them
+ */
+
+
+public class WrapperClass {
+    Integer num = 4;
+    String name = "astra";
+
+    public void concat() {
+        System.out.println(name + num);
+    }
+}
