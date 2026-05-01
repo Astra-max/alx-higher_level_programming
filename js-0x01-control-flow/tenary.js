fc00432c95ba79f0c96f31faa8012eaaf276fb71
Astra-max <waoremaxwel17@gmail.com>
2026-05-01 23:29:50 +0300

    js: nesting tenary operation for additional logic

--- js-0x01-control-flow/tenary.js
+++ js-0x01-control-flow/tenary.js
@@ -1,5 +1,14 @@
 let isActive = true
+let completedTest = true
 
 const color = isActive? "green": "gray"
 
 console.log("User active color: ", color)
+
+/**
+ * tenary operator - can also e nested like nested ifs
+ * 
+ */
+
+const colorWithTest = isActive ? (completedTest? "blue" : "green") : "gray"
+console.log("completed tests color: ",colorWithTest)
