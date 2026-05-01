d26f116aca24e39bd6c10809a41b2c8e304d9768
Astra-max <waoremaxwel17@gmail.com>
2026-05-01 22:29:59 +0300

    js: converting string value to integer

--- js-0x01-str-manipulation/compare.js
+++ js-0x01-str-manipulation/compare.js
@@ -14,3 +14,21 @@ let friends = ['james', 'okello', 'ahn', 'oketch']
 
 let sorted = friends.sort((a,b)=>compare(a,b))
 console.log(sorted)
+
+
+//javascripts stores strings as one code unit
+//surrogate char like emoji requires two code units
+
+let test = "\uD800".isWellFormed()
+console.log(test)
+
+// methods
+// endsWith
+//includes
+//indexOf
+//trimStart
+//trimEnd
+//replace
+//repeat
+//padStart
+//padEnd
