83141825f797c7cd948c56a52fb9af09a90a3d2b
Astra-max <waoremaxwel17@gmail.com>
2026-05-01 20:56:53 +0300

    js: primitive data types

---
+++ js-0x00-variables/data-types.js
@@ -0,0 +1,19 @@
+/**
+ * Primitive types
+ * Number
+ * String
+ * Boolean
+ * undefined
+ * null
+ * BigInt
+ */
+
+
+let number  = 78
+let name  = "maxwel"
+let isMarried  = false
+var salary
+let likesFood = null
+let billion = 984584364281843484938430973874839860980316709194679846
+
+console.log(name, number, isMarried, salary, likesFood, typeof billion)
