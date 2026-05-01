f61fba975f34fe951ac7f2df182246144b298dab
Astra-max <waoremaxwel17@gmail.com>
2026-05-01 23:39:08 +0300

    js: getting elements in array with for of

---
+++ js-0x02-arrays/looping.js
@@ -0,0 +1,8 @@
+let friends = ["james", "evans", "silas", "agnes", "eunice"]
+
+for (let friend of friends) {
+    console.log(`${friend} is my friend`)
+}
+
+// for in retuns index of the elemnts
+// for of return individual value
