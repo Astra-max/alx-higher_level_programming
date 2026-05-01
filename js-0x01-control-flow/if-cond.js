c364ec44de183ae6ea1b8c569fb8d5511d66ab47
Astra-max <waoremaxwel17@gmail.com>
2026-05-01 23:10:33 +0300

    js: if/elseif else conditional statements

---
+++ js-0x01-control-flow/if-cond.js
@@ -0,0 +1,9 @@
+let age = 19
+
+if (age < 18) {
+    console.log("Too young")
+} else if (age > 35) {
+    console.log("sorry you are too old")
+} else {
+    console.log("congrats you are genz")
+}
