fea5f2926a1ed678aa27f0137f4324cf552fa172
Astra-max <waoremaxwel17@gmail.com>
2026-05-01 23:16:13 +0300

    js: switch conditional statement

---
+++ js-0x01-control-flow/switches.js
@@ -0,0 +1,12 @@
+let age = 16
+
+switch (true){
+    case age < 18:
+        console.log("sorry you are too young")
+        break;
+    case age > 35:
+        console.log("sorry you are too old")
+        break
+    default:
+        console.log("congrats you are a genz and you are welcomed")
+}
