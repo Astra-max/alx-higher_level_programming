eae8015045c85ea7da963e1d2e3cc84917b0b67c
Astra-max <waoremaxwel17@gmail.com>
2026-05-01 23:34:48 +0300

    js: short circuiting expression

---
+++ js-0x01-control-flow/short-circuiting.js
@@ -0,0 +1,11 @@
+let demand = 1000
+let allowance = 7000
+let work = "remotely"
+
+demand === 1000 && allowance === 7000
+work === "remotely" || "onsite"
+
+if (demand) {
+    console.log("My demand met")
+}
+console.log(`And all be working ${work}`)
