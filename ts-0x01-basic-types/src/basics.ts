3c5174d091fb28dd662f8db8c96c2f644d6037c6
Astra-max <waoremaxwel17@gmail.com>
2026-05-02 19:31:32 +0000

    ts: typescript primitive data types

---
+++ ts-0x01-basic-types/src/basics.ts
@@ -0,0 +1,5 @@
+let userName: string = "astra";
+let age: number = 26;
+let isMarried: boolean = false;
+
+console.log(`Hello my name is ${userName} am ${age} old and am ${isMarried?"marriade":"not marriade"}`)
