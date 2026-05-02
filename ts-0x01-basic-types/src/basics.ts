fa3a3377f502f4558b06b14ae7df7e6656786e0e
Astra-max <waoremaxwel17@gmail.com>
2026-05-02 19:55:31 +0000

    ts: typescript primitive types/any/null/undefined/unkown

--- ts-0x01-basic-types/src/basics.ts
+++ ts-0x01-basic-types/src/basics.ts
@@ -3,3 +3,27 @@ let age: number = 26;
 let isMarried: boolean = false;
 
 console.log(`Hello my name is ${userName} am ${age} old and am ${isMarried?"marriade":"not marriade"}`)
+
+/**
+ * any - not type safety
+ * unknown - check before using it
+ * null - variable declared, value set to nothing
+ * undefined - variable declared but no value provided
+ */
+
+
+let commingOfChrist: unknown;
+let isSick: boolean;
+let spaceContains = null;
+let holds: any = userName;
+holds = age;
+commingOfChrist = "";
+
+if (typeof commingOfChrist === "string") {
+	commingOfChrist = "soon".toUpperCase();
+} else {
+	commingOfChrist = "sooner";
+}
+
+console.log(`To all christians christ is comming ${commingOfChrist}`);
+console.log("Any makes me sick: ", holds);
