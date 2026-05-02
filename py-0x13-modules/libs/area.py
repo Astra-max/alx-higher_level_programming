f5b1476d5e9747ebee8f35005920b496ab75c1e3
Astra-max <waoremaxwel17@gmail.com>
2026-05-02 22:23:57 +0000

    py: importing square function from package

---
+++ py-0x13-modules/libs/area.py
@@ -0,0 +1,8 @@
+def square(l):
+    return l * l
+
+def rectangle(l, w):
+    return l * w
+
+def circle(r):
+    return 3.142 * r * r
--- py-0x13-modules/main.py
+++ py-0x13-modules/main.py
@@ -1,3 +1,5 @@
 from my_math import *
+from libs.area import square
 
+print("area of a square: ",square(4), "cm3")
 print(add(5,7))
