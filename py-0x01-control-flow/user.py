2779b883fac300b1c449f53282a509358bf64468
Astra-max <waoremaxwel17@gmail.com>
2026-05-02 22:09:10 +0000

    py: program if/else xontrol flow

---
+++ py-0x01-control-flow/user.py
@@ -0,0 +1,8 @@
+age = int(input("Enter you age: "))
+
+if age < 18:
+    print("sorry you are still young")
+elif age > 50:
+    print("sorry you are too old")
+else:
+    print("welcome on board youngkeee")
