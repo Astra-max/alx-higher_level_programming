77574cc0f8caf2fef949b1aa725439ca3f68c63d
Astra-max <waoremaxwel17@gmail.com>
2026-05-04 23:08:46 +0300

    ts: passing and returning functions from function/HoC/F

--- ts-0x03-fuctions/src/optional-para.ts
+++ ts-0x03-fuctions/src/optional-para.ts
@@ -1,3 +1,12 @@
+/**
+ * optional/default values should be passed at end of function param list
+ * @param music 
+ * @param artist - optional param
+ * @returns -- void
+ */
+
+
+
 function playing(music: string, artist?: string) {
     if (music && artist) {
         console.log(`playing ${music} by ${artist}`)
