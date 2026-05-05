a3a1381669a4a48647e7fcdc849a42f5ceed0042
Astra-max <waoremaxwel17@gmail.com>
2026-05-05 23:29:21 +0300

    js: getting current date in javascript

---
+++ js-0x05-date-datetime/create-date.js
@@ -0,0 +1,13 @@
+function currentDate() {
+    const dateNow = new Date()
+    console.log(dateNow)
+}
+currentDate()
+
+// Date() return string rep of current date
+// return type is string
+// lacks method to date object
+// if values is passwed to function they are ignore
+
+// new Date() create the object
+// return type is .ISODateString
