2d82832ba6641081bec820ea2bb6c80a3eb6105d
Astra-max <waoremaxwel17@gmail.com>
2026-05-07 13:30:15 +0300

    js: appending to an array

---
+++ js-0x02-arrays/appending.js
@@ -0,0 +1,9 @@
+import { ages } from "./methods.js"
+
+// adding infrom of the array
+
+const newAges = ages.unshift(23, 45) // return is new length of array
+ages.push(30) // appends value at the end of list
+
+ages.pop() // remove from end return the array
+ages.shift() // removes first element from the array and return the array
