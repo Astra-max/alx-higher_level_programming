b8c8d1dfb8e3d9d8abcd33ad5a1303ceb2b7cda8
Astra-max <waoremaxwel17@gmail.com>
2026-05-07 13:02:53 +0300

    js: sorting string and number arrays

--- js-0x02-arrays/sorting.js
+++ js-0x02-arrays/sorting.js
@@ -4,9 +4,36 @@ import { ages } from "./methods.js"
  * sorting - in a particular order
  */
 
+
+// sort -- converts the array to string
+// a - b -- forces the values to numbers
+
 const asc = (a,b) => a - b;
 
 const newlySorted = ages.sort(asc)
+
+console.log(newlySorted)
 const descOrder = ages.reverse()
 console.log(descOrder)
-console.log(newlySorted)
+
+const friends = ['joseph', 'okello', 'oketch', 'adongo', 'wairachu']
+
+/**
+ * perfect way of sorting string array 
+ * alternative to go character by character sort
+ */
+
+const sortStr = (a,b) => a.length - b.length // all fields must be strings
+
+/**
+ * sorting via localecompare function
+ */
+
+function sortCompare(a, b) {
+    if (a > b) return 1
+    if (a < b) return -1
+    return 0
+}
+
+const sortedFriends = friends.sort(sortCompare)
+console.log(sortedFriends)
