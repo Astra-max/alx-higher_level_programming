915bad4d4e6a828e8d1fad52205eb30482442319
Astra-max <waoremaxwel17@gmail.com>
2026-05-07 12:43:08 +0300

    js: summing all array members via reduce method

---
+++ js-0x02-arrays/methods.js
@@ -0,0 +1,7 @@
+export const ages = [2,3,4,5,6,4,1]
+
+const totalAge = ages.reduce((a,b)=> a+b) // summing all values in arrray
+// mapping through an array returns new modified array
+
+console.log(`Total users ages: ${totalAge}`)
+console.log(`Odd users ages: ${oddAges}`)
