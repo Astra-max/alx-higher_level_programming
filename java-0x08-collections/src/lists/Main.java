3f195bc7dc602aaaabddfd739342f84f781b4b62
Astra-max <waoremaxwel17@gmail.com>
2026-05-13 23:08:59 +0300

    java: adding values to a priority que

---
+++ java-0x08-collections/src/lists/Main.java
@@ -0,0 +1,11 @@
+package lists;
+public class Main {
+    public static void main(String[] args) {
+        MyLists mylist = new MyLists();
+        mylist.add("astra");
+        mylist.add("james");
+        mylist.add("john");
+        mylist.remove(2);
+        mylist.print();
+    }
+}
--- java-0x08-collections/src/lists/MyQueue.java
+++ java-0x08-collections/src/lists/MyQueue.java
@@ -1,4 +1,4 @@
-package src.lists;
+package lists;
 
 import java.util.LinkedList;
 import java.util.Queue;
