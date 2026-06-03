f2bb86216e1288a01df16c849ae13146ea233975
Astra-max <waoremaxwel17@gmail.com>
2026-06-03 23:17:53 +0300

    java: loose string equality check ignoring cases sensitivity

--- java-0x0A-str/src/StrComp.java
+++ java-0x0A-str/src/StrComp.java
@@ -2,14 +2,19 @@ public class StrComp {
 	/**
 	 * strinhs are referenced types in java
 	 */
-	private final String user1 = "james";
+	private final String user1 = "John";
 	private final String user2 = "john";
 	public static void main(String[] args) {
 		StrComp str = new StrComp();
-		System.out.println("loose equality: " + str.equal());
+		System.out.println("strict equality: " + str.equal());
+		System.out.println("loose equality check: "+ str.ignoreCase());
 	}
 
 	public boolean equal() {
 		return user1.equals(user2);
+	}
+
+	public boolean ignoreCase() {
+		return user1.equalsIgnoreCase(user2);
 	}
 }
