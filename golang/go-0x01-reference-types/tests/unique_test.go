132d0366dd0516d1b7fee6bb4ea6ecbb0f19065e
Astra-max <waoremaxwel17@gmail.com>
2026-09-07 00:42:34 +0300

    tested: function that returns unique values from an array

---
+++ golang/go-0x01-reference-types/tests/unique_test.go
@@ -0,0 +1,20 @@
+package tests
+
+import (
+	"reference/data"
+	"reference/libs"
+	"reflect"
+	"testing"
+)
+
+func Test_Unique(t *testing.T) {
+	for _, tt := range data.UniqueNumbersTestCases {
+		t.Run(tt.Name, func(t *testing.T) {
+			got := libs.UniqueNumbers(tt.Input)
+
+			if !reflect.DeepEqual(got, tt.Expected) {
+				t.Errorf("Expected %v but got %v\n", tt.Expected, got)
+			}
+		})
+	}
+}
