d82bd496b63bddd391e8d73456aad9689a605257
Astra-max <waoremaxwel17@gmail.com>
2026-09-10 22:58:20 +0300

    feat: solving finding common by brutal force approach

--- golang/go-0x01-reference-types/libs/common.go
+++ golang/go-0x01-reference-types/libs/common.go
@@ -4,7 +4,7 @@ func FindCommon(a, b []int) []int {
 	results := []int{}
 
 	for i:=0; i<=len(a)-1; i++ {
-		for j:=0; j<=len(b)-1; j++ {
+		for j:=i+1; j<=len(b)-1; j++ {
 			if a[i] == a[j] {
 				results = append(results, a[i])
 			}
